6e1daa35671f44063a70136c855bc549db3b33bc
idkcode22 <appleuser828837@gmail.com>
2025-12-02 19:51:45 -0800

    Added follow after cut positioning logic.

--- HitsoundTweaks/HarmonyPatches/NoteCutSoundEffect_Transform_Position_Patch.cs
+++ HitsoundTweaks/HarmonyPatches/NoteCutSoundEffect_Transform_Position_Patch.cs
@@ -1,9 +1,10 @@
-﻿using HarmonyLib;
-using HitsoundTweaks.Configuration;
-using SiraUtil.Affinity;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using HitsoundTweaks.Configuration;
+using SiraUtil.Affinity;
 using UnityEngine;
 
 namespace HitsoundTweaks.HarmonyPatches;
@@ -12,6 +13,23 @@ namespace HitsoundTweaks.HarmonyPatches;
  * These patches allow the user to configure whether or not the spatialized hitsound should follow the sabers
  * If disabled, the hitsound will always play at the player's feet
  */
+internal static class NoteCutSoundEffectExtensions
+{
+    // Store fade data for each NoteCutSoundEffect instance
+    // Using a table to separate data from the original class
+    private static readonly ConditionalWeakTable<NoteCutSoundEffect, FadeData> fadeTable = new();
+
+    public class FadeData
+    {
+        public Vector3 startPos;
+        public double startTime;
+        public float duration;
+        public bool active;
+    }
+
+    public static FadeData GetFadeData(this NoteCutSoundEffect instance)
+        => fadeTable.GetOrCreateValue(instance);
+}
 internal class NoteCutSoundEffect_Transform_Position_NoteWasCut_Patch : IAffinity
 {
     private readonly PluginConfig config;
@@ -46,27 +64,29 @@ internal class NoteCutSoundEffect_Transform_Position_NoteWasCut_Patch : IAffinit
     // set transform position if desired
     [AffinityPatch(typeof(NoteCutSoundEffect), nameof(NoteCutSoundEffect.NoteWasCut))]
     private void Postfix(NoteCutSoundEffect __instance, AudioSource ____audioSource, NoteController
-        ____noteController, bool ____goodCut, NoteController noteController, in NoteCutInfo noteCutInfo)
+        ____noteController, bool ____goodCut, NoteController noteController, in NoteCutInfo noteCutInfo, Saber ____saber)
     {
         if (____noteController != noteController)
         {
             return;
         }
-
-        if (!____goodCut)
-        {
-            ____audioSource.spatialize = true;
-            __instance.transform.position = noteCutInfo.cutPoint;
-            return;
-        }
-
         if (!config.StaticSoundPos && ____audioSource.spatialize)
         {
+
+            // Apply the transformed position
             __instance.transform.position = noteCutInfo.cutPoint;
+            if (config.followAfterCut)
+            {
+                var fade = __instance.GetFadeData();
+                fade.startPos = noteCutInfo.cutPoint;
+                fade.startTime = AudioSettings.dspTime;
+                fade.duration = (0.434f / ____audioSource.pitch); // the amount of time in seconds it takes for the sound to fully follow the saber.
+                fade.active = true;
+            }
+
         }
     }
 }
-
 internal class NoteCutSoundEffect_Transform_Position_LateUpdate_Patch : IAffinity
 {
     private readonly PluginConfig config;
@@ -100,14 +120,41 @@ internal class NoteCutSoundEffect_Transform_Position_LateUpdate_Patch : IAffinit
     }
 
     [AffinityPatch(typeof(NoteCutSoundEffect), nameof(NoteCutSoundEffect.OnLateUpdate))]
-    private void Postfix(bool ____noteWasCut, Saber ____saber, AudioSource ____audioSource, NoteCutSoundEffect
-        __instance)
+    private void Postfix(NoteCutSoundEffect __instance, AudioSource ____audioSource, Saber ____saber, bool ____noteWasCut)
     {
-        // set transform position if desired
         if (!____noteWasCut && !config.StaticSoundPos && ____audioSource.spatialize)
         {
-            __instance.transform.position = ____saber.saberBladeTopPos;
+            __instance.transform.position = ____saber.saberBladeTopPosForLogic;
         }
+        if (____audioSource.spatialize && !config.StaticSoundPos && config.followAfterCut)
+        {
+            // Get live saber midpoint positioning.
+            Vector3 liveMidSaberPos = (____saber.saberBladeTopPosForLogic + ____saber.saberBladeBottomPosForLogic) / 2;
+            // If fading, lerp from cutPoint to saber position over time
+            var fade = __instance.GetFadeData();
+            if (fade.active)
+            {
+                double elapsed = AudioSettings.dspTime - fade.startTime;
+                float t = Mathf.Clamp01((float)elapsed / fade.duration);
+                t = Mathf.SmoothStep(0f, 1f, t);
+
+                // Start near cutPoint, end up tracking the live saber position
+                Vector3 lerpedPos = Vector3.Lerp(fade.startPos, liveMidSaberPos, t);
+                __instance.transform.position = lerpedPos;
+
+                // Once fade fully completes, permanently follow saber
+                if (t >= 1f)
+                {
+                    fade.active = false;
+                }
+            }
+            else if (!fade.active)
+            {
+                // Continue following saber after fade completes
+                __instance.transform.position = liveMidSaberPos;
+            }
+        }
+
     }
 }
 
@@ -146,7 +193,7 @@ internal class NoteCutSoundEffect_Transform_Position_Init_Patch : IAffinity
         // not sure how necessary this is, but since the game does it I might as well too
         if (!config.StaticSoundPos && ____audioSource.spatialize)
         {
-            __instance.transform.position = saber.saberBladeTopPos;
+            __instance.transform.position = saber.saberBladeTopPosForLogic;
         }
     }
 }
